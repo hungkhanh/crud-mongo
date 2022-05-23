5867a6e31ee80a894d9feac0f1e2fb1951b13575
hungpk <hungpk1412@gmail.com>
2022-05-23 11:16:24 +0700

    test api

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+// test api
+app.get('/', (req, res) => {
+  res.status(200).json({
+    statusCode: 200,
+    success: true,
+    message: 'hello from api',
+  });
+});
+
+// port
+const PORT = process.env.PORT || 3000;
+
+// server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
